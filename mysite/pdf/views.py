6f4bec329ef2be1dd6d4dacb441c8ef6daa68a89
Nitish Gautam <nitishgautam.uob.com>
2020-08-30 21:55:06 +0100

    initial functional commit

---
+++ mysite/pdf/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render
+from .models import Profile
+import pdfkit
+from django.http import HttpResponse
+from django.template import loader
+import io
+# Create your views here.
+
+
+def accept(request):
+    if request.method == "POST":
+        name = request.POST.get("name", "")
+        email = request.POST.get("email", "")
+        phone = request.POST.get("phone", "")
+        summary = request.POST.get("summary", "")
+        degree = request.POST.get("degree", "")
+        school = request.POST.get("school", "")
+        university = request.POST.get("university", "")
+        work_experience = request.POST.get("work_experience", "")
+        skills = request.POST.get("skills", "")
+
+        profile = Profile(name=name, email=email, phone=phone, summary=summary, degree=degree,
+                          school=school, university=university, work_experience=work_experience, skills=skills)
+        profile.save()
+
+    return render(request, 'pdf/accept.html')
+
+
+def resume(request, pk):
+    user_profile = Profile.objects.get(pk=pk)
+    template = loader.get_template('pdf/resume.html')
+    html = template.render({'user_profile': user_profile})
+    options = {
+        'page-size': 'Letter',
+        'encoding': "UTF-8",
+    }
+    pdf = pdfkit.from_string(html, False, options)
+    response = HttpResponse(pdf, content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment'
+    filename = "resume.pdf"
+    return response
+
+
+'''
+def resume(request, pk):
+    user_profile = Profile.objects.get(pk=pk)
+    print('pk', pk)
+    return render(request, 'pdf/resume.html', {'user_profile': user_profile})
+'''
+
+def list(request):
+    profiles = Profile.objects.all()
+    return render(request, 'pdf/list.html', {'profiles': profiles})
